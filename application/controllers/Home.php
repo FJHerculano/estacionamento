284b974da112194e29a7ae6ab0c1fd38f35277bf
fjherculano <herculano@aluno.fapce.edu.br>
2021-11-28 23:36:01 -0300

    first commit

---
+++ application/controllers/Home.php
@@ -0,0 +1,18 @@
+		$this->load->view('layout/footer');
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function index()
+	{
+
+		$data = array(
+			'titulo' => 'Home'
+		);
+
+		$this->load->view('layout/header', $data);
+		$this->load->view('home/index');
+		$this->load->view('layout/footer');
+	}
+}
